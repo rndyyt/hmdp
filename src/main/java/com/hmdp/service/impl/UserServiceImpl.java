c7c0227867f344b2f47116688f285bd3ec71ab34
rndyyt <1972245397@qq.com>
2025-12-08 18:32:08 +0800

    完成登录功能

--- src/main/java/com/hmdp/service/impl/UserServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/UserServiceImpl.java
@@ -1,22 +1,30 @@
 package com.hmdp.service.impl;
 
+import cn.hutool.core.bean.BeanUtil;
+import cn.hutool.core.bean.copier.CopyOptions;
+import cn.hutool.core.lang.UUID;
 import cn.hutool.core.util.RandomUtil;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.hmdp.dto.LoginFormDTO;
 import com.hmdp.dto.Result;
+import com.hmdp.dto.UserDTO;
 import com.hmdp.entity.User;
 import com.hmdp.mapper.UserMapper;
 import com.hmdp.service.IUserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
 
 
 import javax.servlet.http.HttpSession;
 import java.time.Duration;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.TimeUnit;
+
+import static com.hmdp.utils.RedisConstants.*;
+import static com.hmdp.utils.SystemConstants.USER_NICK_NAME_PREFIX;
 
 /**
  * <p>
@@ -39,7 +47,7 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements IU
         String code = RandomUtil.randomNumbers(6);
 
         // 2.保存验证码到redis
-        stringRedisTemplate.opsForValue().set("code:" + phone, code, Duration.ofMinutes(5));
+        stringRedisTemplate.opsForValue().set("code:" + phone, code, LOGIN_CODE_TTL, TimeUnit.MINUTES);
 
         // 3.发送验证码
         log.info("发送短信验证码成功，验证码：{}", code);
@@ -63,10 +71,21 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements IU
             // 不存在，创建新用户并保存
             user = createUserWithPhone(phone);
         }
-        // 保存用户信息到redis中
+        String token = UUID.randomUUID().toString(true);
+        // 将User转化为UserDTO，其目的在于去除敏感或无用字段，仅保留业务所需的核心字段
+        UserDTO userDTO = BeanUtil.copyProperties(user, UserDTO.class);
+        Map<String,Object> userMap = BeanUtil.beanToMap(userDTO,new HashMap<>(), CopyOptions.create()
+                .setIgnoreNullValue(true).setFieldValueEditor((fieldName, fieldValue) -> fieldValue.toString()));
+        String tokenKey = LOGIN_USER_KEY + token;
+        stringRedisTemplate.opsForHash().putAll(tokenKey, userMap);
+        stringRedisTemplate.expire(tokenKey,LOGIN_USER_TTL,TimeUnit.MINUTES);
 
+        return Result.ok(token);
     }
 
     private User createUserWithPhone(String phone) {
+        User user = new User().setPhone(phone).setNickName(USER_NICK_NAME_PREFIX + RandomUtil.randomString(10));
+        save(user);
+        return user;
     }
 }
